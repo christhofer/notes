0f3c13eaefade7304bf5fcfb8f3ccad57f197a08
Christhofer Natalius <christhofernatalius@gmail.com>
2021-11-13 09:26:12 +0700

    setup env and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  
+  return defineConfig({
+    plugins: [vue()],
+    server: {
+      host: process.env.VITE_SERVER_HOST,
+      port: process.env.VITE_SERVER_PORT ?? 3000,
+      https: process.env.VITE_SERVER_HTTPS_CERT ? {
+        cert: process.env.VITE_SERVER_HTTPS_CERT,
+        key: process.env.VITE_SERVER_HTTPS_KEY,
+        ciphers: process.env.VITE_SERVER_HTTPS_CIPHERS,
+      } : false,
+    },
+    define: {
+      '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
+    },
+    build: {
+      outDir: 'dist/' + process.env.npm_package_version,
+      sourcemap: true,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  })
+}
